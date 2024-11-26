23a2e6b9a9a6a4670b0cf02de9224a2a2c40ffbb
Swizec Teller <swizec@swizec.com>
2024-11-26 14:59:32 -0800

    Load fingerprinted entry files in prod mode

--- src/flask_vite/tags.py
+++ src/flask_vite/tags.py
@@ -4,6 +4,8 @@
 
 from __future__ import annotations
 
+import glob
+from pathlib import Path
 from textwrap import dedent
 import re
 
@@ -18,14 +20,22 @@ def make_tag(*, static: bool = False):
         tag = make_debug_tag()
     return Markup(tag)
 
-
+# Find fingerprinted files that match declared entry points
 def make_static_tag():
     vite_entry_points = current_app.extensions["vite"].vite_entry_points
-    # js_file = Path(glob.glob(f"{vite_folder_path}/dist/assets/*.js")[0]).name
-    # css_file = Path(glob.glob(f"{vite_folder_path}/dist/assets/*.css")[0]).name
-
-    js_files = [re.sub(r'\..+$', '.js', src) for src in vite_entry_points]
-    script_tags = '\n'.join(f"<script type='module' src='{url_for('vite.static', filename=src)}'></script>" for src in js_files)
+    vite_folder_path = current_app.extensions["vite"].vite_folder_path
+    
+    js_files = glob.glob(f"{vite_folder_path}/dist/assets/*.js")
+    entry_points = [re.sub(r'\..+$', '', src) for src in vite_entry_points]
+    
+    entry_files = [
+        Path(js_file).name 
+        for js_file in js_files 
+        for entry in entry_points 
+        if Path(js_file).name.startswith(entry)
+    ]
+    
+    script_tags = '\n'.join(f"<script type='module' src='{url_for('vite.static', filename=src)}'></script>" for src in entry_files)
 
     return dedent(
         f"""
